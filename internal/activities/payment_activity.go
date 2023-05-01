cd5c834aff8796a415a0072d8f0cda0f5a974c5c
w31708 <mohamed.macow@ufst.dk>
2023-05-01 13:27:31 +0200

    Status as enum

--- internal/activities/payment_activity.go
+++ internal/activities/payment_activity.go
@@ -8,15 +8,45 @@ import (
 
 type PaymentActivity struct {
 	transaction_id uuid.UUID
-	state          string
+	status         PaymentState
 	amount         int
 	user           uuid.UUID
 }
 
-func (a *PaymentActivity) CheckBalance(ctx context.Context, param PaymentActivity) {
+type PaymentState string
+
+const (
+	Unknown   PaymentState = ""
+	Created   PaymentState = "created"
+	Ready     PaymentState = "ready"
+	Reserved  PaymentState = "reserved"
+	Withdrawn PaymentState = "withdrawn"
+)
+
+func (a *PaymentActivity) StartTransaction(ctx context.Context, param *PaymentActivity) (*PaymentActivity, error) {
+	param.transaction_id = uuid.New()
+	param.status = Created
+
+	return param, nil
 
 }
 
-func (a *PaymentActivity) ReserveFunds(ctx context.Context, param PaymentActivity)
+func (a *PaymentActivity) CheckBalance(ctx context.Context, param *PaymentActivity) (*PaymentActivity, error) {
+	// Check in repo if TX is active, otherwise can -> Based on single or combinaion of keys
+	// Card details, sub from JWT or something else
+	param.status = Ready
 
-func (a *PaymentActivity) WithdrawFunds(ctx context.Context, param PaymentActivity)
+	return param, nil
+
+}
+
+func (a *PaymentActivity) ReserveFunds(ctx context.Context, param PaymentActivity) (*PaymentActivity, error) {
+	param.status = Reserved
+	return &param, nil
+}
+
+func (a *PaymentActivity) WithdrawFunds(ctx context.Context, param PaymentActivity) (*PaymentActivity, error) {
+	param.status = Withdrawn
+
+	return &param, nil
+}
