3dd7841656a775990e8caaef7859f482125569a4
Mohamed Macow <mmacovv@gmail.com>
2023-05-15 21:54:18 +0200

    brrr

--- internal/models/payment_model.go
+++ internal/models/payment_model.go
@@ -1,21 +1,27 @@
 package models
 
+type Item struct {
+	Reference        string `json:"reference"`
+	Name             string `json:"name"`
+	Quantity         int    `json:"quantity"`
+	Unit             string `json:"unit"`
+	UnitPrice        int    `json:"unitPrice"`
+	GrossTotalAmount int    `json:"grossTotalAmount"`
+	NetTotalAmount   int    `json:"netTotalAmount"`
+}
+
+type Order struct {
+	Items    []Item `json:"items"`
+	Amount   int    `json:"amount"`
+	Currency string `json:"currency"`
+}
+
+type Checkout struct {
+	URL      string `json:"url"`
+	TermsURL string `json:"termsUrl"`
+}
+
 type NetsPayment struct {
-	Order struct {
-		Items []struct {
-			Reference        string `json:"reference"`
-			Name             string `json:"name"`
-			Quantity         int    `json:"quantity"`
-			Unit             string `json:"unit"`
-			UnitPrice        int    `json:"unitPrice"`
-			GrossTotalAmount int    `json:"grossTotalAmount"`
-			NetTotalAmount   int    `json:"netTotalAmount"`
-		} `json:"items"`
-		Amount   int    `json:"amount"`
-		Currency string `json:"currency"`
-	} `json:"order"`
-	Checkout struct {
-		URL      string `json:"url"`
-		TermsURL string `json:"termsUrl"`
-	} `json:"checkout"`
+	Order    Order    `json:"order"`
+	Checkout Checkout `json:"checkout"`
 }
--- internal/services/nets_client.go
+++ internal/services/nets_client.go
@@ -1,1 +1,38 @@
 package services
+
+import "github.com/go-temporal-laboratory/internal/models"
+
+func NetsCreatePayment() models.NetsPayment {
+	payment := models.NetsPayment{
+		Order: models.Order{
+			Items: []models.Item{
+				{
+					Reference:        "Ref1234",
+					Name:             "Product Name",
+					Quantity:         2,
+					Unit:             "pcs",
+					UnitPrice:        100,
+					GrossTotalAmount: 200,
+					NetTotalAmount:   180,
+				},
+				{
+					Reference:        "Ref5678",
+					Name:             "Another Product",
+					Quantity:         1,
+					Unit:             "pcs",
+					UnitPrice:        50,
+					GrossTotalAmount: 50,
+					NetTotalAmount:   45,
+				},
+			},
+			Amount:   250,
+			Currency: "USD",
+		},
+		Checkout: models.Checkout{
+			URL:      "https://checkout.example.com",
+			TermsURL: "https://checkout.example.com/terms",
+		},
+	}
+
+	return payment
+}
